b9fbf2913a15f8168b100f8948385d5fa9ba4a09
MadFox <otaku.suprem@gmail.com>
2018-09-26 14:07:44 +0200

    init project

---
+++ Assets/Scripts/ScriptTracker.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ScriptTracker : MonoBehaviour, ITrackableEventHandler
+
+{
+    TrackableBehaviour mTrackableBehaviour;
+    VuMarkManager mVuMarkManager;
+
+    public VuMarkTarget vumark;
+
+    public GameObject obj1;
+    public GameObject obj2;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour != null)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        mVuMarkManager = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackerFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackerLost();
+        }
+    }
+
+    void OnTrackerFound()
+    {
+        foreach (var item in mVuMarkManager.GetActiveBehaviours())
+        {
+            int targetObj = System.Convert.ToInt32 (item.VuMarkTarget.InstanceId.NumericValue);
+            transform.GetChild(targetObj - 1).gameObject.SetActive(true);   
+        }
+    }
+
+    void OnTrackerLost()
+    {
+        foreach (var item in mVuMarkManager.GetActiveBehaviours())
+        {
+            int targetObj = System.Convert.ToInt32 (item.VuMarkTarget.InstanceId.NumericValue);
+            transform.GetChild(targetObj - 1).gameObject.SetActive(false);
+        }
+    }
+}
