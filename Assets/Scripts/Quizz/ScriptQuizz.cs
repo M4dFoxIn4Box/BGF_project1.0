132f64cf485f2fae08236e70c18b90d55367b877
Pierre <pierrebatisse33130@gmail.com>
2018-09-28 15:47:03 +0200

    Ajout ScriptableObj pour Quizz

---
+++ Assets/Scripts/Quizz/ScriptQuizz.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptQuizz : MonoBehaviour {
+
+    public Text quizzText;
+    public Text answer1;
+    public Text answer2;
+    public Text answer3;
+    public Text answer4;
+
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+
+    private string answer1Text;
+    private string answer2Text;
+    private string answer3Text;
+    private string answer4Text;
+
+    //public string questionText;
+    public ScriptableQuizz scriptableQuizzClass;
+
+    // Use this for initialization
+    void Start () {
+
+        quizzText.text = scriptableQuizzClass.quizzQuestion;
+        answer1Text = scriptableQuizzClass.answer1;
+        answer2Text = scriptableQuizzClass.answer2;
+        answer3Text = scriptableQuizzClass.answer3;
+        answer4Text = scriptableQuizzClass.answer4;
+
+        answer1.text = answer1Text;
+        answer2.text = answer2Text;
+        answer3.text = answer3Text;
+        answer4.text = answer4Text;
+
+        button1.onClick.AddListener(taskOnClick1);
+        button2.onClick.AddListener(taskOnClick2);
+        button3.onClick.AddListener(taskOnClick3);
+        button4.onClick.AddListener(taskOnClick4);
+
+
+    }
+
+	
+    public void taskOnClick1() // BOUTON 1
+    {
+        if (scriptableQuizzClass.rightAnswer == 1)
+        {
+            button1.GetComponent<Image>().color = Color.green;
+            button2.GetComponent<Image>().color = Color.red;
+            button3.GetComponent<Image>().color = Color.red;
+            button4.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            button1.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public void taskOnClick2() // BOUTON 2
+    {
+        if (scriptableQuizzClass.rightAnswer == 2)
+        {
+            button1.GetComponent<Image>().color = Color.red;
+            button2.GetComponent<Image>().color = Color.green;
+            button3.GetComponent<Image>().color = Color.red;
+            button4.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            button2.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public void taskOnClick3() // BOUTON 3
+    {
+        if (scriptableQuizzClass.rightAnswer == 3)
+        {
+            button1.GetComponent<Image>().color = Color.red;
+            button2.GetComponent<Image>().color = Color.red;
+            button3.GetComponent<Image>().color = Color.green;
+            button4.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            button3.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public void taskOnClick4() // BOUTON 4
+    {
+        if (scriptableQuizzClass.rightAnswer == 4)
+        {
+            button1.GetComponent<Image>().color = Color.red;
+            button2.GetComponent<Image>().color = Color.red;
+            button3.GetComponent<Image>().color = Color.red;
+            button4.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            button4.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Quizz/ScriptableQuizz.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuizzQuestion", menuName = "Inventory/Question", order = 1)]
+
+public class ScriptableQuizz : ScriptableObject {
+        
+    public string quizzName = "newQuizz";
+    public string quizzQuestion;
+    public string answer1;
+    public string answer2;
+    public string answer3;
+    public string answer4;
+    [Tooltip("1 correspond à answer1, etc")]
+    public int rightAnswer; // 1 à 4, 1 correspondant à answer1, etc...
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
