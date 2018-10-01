afea85a77ef0a3e1e61e32a10ce624b87f02d860
Pierre <pierrebatisse33130@gmail.com>
2018-10-01 13:53:25 +0200

    Ajout funfact, trophée

--- Assets/Scripts/Quizz/ScriptQuizz.cs
+++ Assets/Scripts/Quizz/ScriptQuizz.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class ScriptQuizz : MonoBehaviour {
 
     public Image questionBackground;
+    public Image screenBackground;
 
     public Canvas quizzCanvas;
+    public GameObject quizzOnlyPanel;
     public Button leaveCanvas;
 
     public Text quizzText;
@@ -15,16 +17,27 @@ public class ScriptQuizz : MonoBehaviour {
     public Text answer2;
     public Text answer3;
     public Text answer4;
+    public Text funFact;
 
     public Button button1;
     public Button button2;
     public Button button3;
     public Button button4;
 
+    private float r = 0.4509804f;
+    private float g = 0.4509804f;
+    private float b = 0.4509804f;
+
     private string answer1Text;
     private string answer2Text;
     private string answer3Text;
     private string answer4Text;
+    private string funFactText;
+
+    public GameObject congratulationsImage;
+    public Image trophyImage;
+
+    private bool isAnswered = false;
 
     //public string questionText;
     public ScriptableQuizz scriptableQuizzClass;
@@ -38,24 +51,25 @@ public class ScriptQuizz : MonoBehaviour {
         answer2Text = scriptableQuizzClass.answer2;
         answer3Text = scriptableQuizzClass.answer3;
         answer4Text = scriptableQuizzClass.answer4;
+        funFactText = scriptableQuizzClass.funFact;
 
         answer1.text = answer1Text;
         answer2.text = answer2Text;
         answer3.text = answer3Text;
         answer4.text = answer4Text;
+        funFact.text = funFactText;
 
-        button1.onClick.AddListener(taskOnClick1);
-        button2.onClick.AddListener(taskOnClick2);
-        button3.onClick.AddListener(taskOnClick3);
-        button4.onClick.AddListener(taskOnClick4);
+        button1.onClick.AddListener(TaskOnClick1);
+        button2.onClick.AddListener(TaskOnClick2);
+        button3.onClick.AddListener(TaskOnClick3);
+        button4.onClick.AddListener(TaskOnClick4);
 
         leaveCanvas.onClick.AddListener(LeaveQuizz);
-
 
     }
 
 	
-    public void taskOnClick1() // BOUTON 1
+    public void TaskOnClick1() // BOUTON 1
     {
         if (scriptableQuizzClass.rightAnswer == 1)
         {
@@ -73,7 +87,7 @@ public class ScriptQuizz : MonoBehaviour {
         }
     }
 
-    public void taskOnClick2() // BOUTON 2
+    public void TaskOnClick2() // BOUTON 2
     {
         if (scriptableQuizzClass.rightAnswer == 2)
         {
@@ -91,7 +105,7 @@ public class ScriptQuizz : MonoBehaviour {
         }
     }
 
-    public void taskOnClick3() // BOUTON 3
+    public void TaskOnClick3() // BOUTON 3
     {
         if (scriptableQuizzClass.rightAnswer == 3)
         {
@@ -109,7 +123,7 @@ public class ScriptQuizz : MonoBehaviour {
         }
     }
 
-    public void taskOnClick4() // BOUTON 4
+    public void TaskOnClick4() // BOUTON 4
     {
         if (scriptableQuizzClass.rightAnswer == 4)
         {
@@ -127,9 +141,30 @@ public class ScriptQuizz : MonoBehaviour {
         }
     }
 
+
     public void LeaveQuizz()
     {
         quizzCanvas.enabled = false;
+
+        if (isAnswered == true)
+        {
+            quizzOnlyPanel.SetActive(false);
+            screenBackground.enabled = false;
+            
+
+        }
+
+        button1.GetComponent<Image>().color = new Color(r,g,b);
+        button2.GetComponent<Image>().color = new Color(r,g,b);
+        button3.GetComponent<Image>().color = new Color(r,g,b);
+        button4.GetComponent<Image>().color = new Color(r,g,b);
+
+        questionBackground.color = new Color(0.7254902f, 0.7254902f, 0.7254902f);
+
+        button1.interactable = true;
+        button2.interactable = true;
+        button3.interactable = true;
+        button4.interactable = true;
     }
 
     public void RightAnswer()
@@ -139,11 +174,22 @@ public class ScriptQuizz : MonoBehaviour {
         button3.interactable = false;
         button4.interactable = false;
 
+        congratulationsImage.SetActive(true);
+
         questionBackground.color = Color.green;
+
+        isAnswered = true;
     }
 
 
     void Update () {
-		
-	}
+
+        if (Input.GetKeyDown("q"))
+        {
+
+            quizzCanvas.enabled = true;
+
+        }
+
+    }
 }
