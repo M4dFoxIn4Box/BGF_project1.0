5df63cf89d1da52b520fec932bdf49e6de1ab1a4
MSI\Lucas <lucasblon@orange.fr>
2018-10-02 09:58:22 +0200

    il e bo erik

--- Assets/Scripts/Mini_Jeux/Chamboule/ScriptableMiniJeux.cs
+++ Assets/Scripts/Mini_Jeux/Chamboule/ScriptableMiniJeux.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "MiniJeux", menuName = "Scriptable/MiniJeux", order = 1)]
 
-public class ScriptableChamboule : ScriptableObject {
+public class ScriptableChamboule : ScriptableObject
+{
 
     public GameObject prefabMiniJeux;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
