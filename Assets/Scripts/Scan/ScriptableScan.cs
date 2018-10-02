3dd25087765ba09e5a03d138f8ae948730aa912d
MadFox <otaku.suprem@gmail.com>
2018-10-02 11:10:59 +0200

    Scriptable scan

--- Assets/Scripts/Scan/ScriptableScan.cs
+++ Assets/Scripts/Scan/ScriptableScan.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Scan", menuName = "Inventory/Scan", order = 1)]
+[CreateAssetMenu(fileName = "Scan", menuName = "Scriptable Scan/Scan", order = 1)]
 
 public class ScriptableScan : MonoBehaviour {
 
