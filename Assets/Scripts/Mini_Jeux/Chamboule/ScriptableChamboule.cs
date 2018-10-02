08f956dec0cd650bd70594115aa0183c97c05f7e
MSI\Lucas <lucasblon@orange.fr>
2018-10-02 09:43:38 +0200

    scriptable chamboule tout

---
+++ Assets/Scripts/Mini_Jeux/Chamboule/ScriptableChamboule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MiniJeux", menuName = "Scriptable/MiniJeux", order = 1)]
+
+public class ScriptableChamboule : ScriptableObject {
+
+    public GameObject prefabMiniJeux;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
