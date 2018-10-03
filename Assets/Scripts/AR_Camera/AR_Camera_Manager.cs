2a83de8764e1a6e8a5208e08fedca2da27108d0a
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-10-03 11:03:05 +0200

    AR camera + scene proto

---
+++ Assets/Scripts/AR_Camera/AR_Camera_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_Camera_Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<VuforiaMonoBehaviour>().enabled = false;
+        GetComponent<DefaultInitializationErrorHandler>().enabled = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
