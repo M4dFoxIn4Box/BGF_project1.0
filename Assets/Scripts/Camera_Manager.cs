82e0e8ecef0c74588da2daa83118a8f44be5bfc7
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-10-22 14:41:59 +0200

    main canvas + script camera manager + ui share scalled + 2 cameras dans a scene build + scene build

---
+++ Assets/Scripts/Camera_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Manager : MonoBehaviour
+{
+
+    public GameObject cameraAR;
+    public Camera mainCamera;
+    public Transform spanwPointCamera;
+    public GameObject vumarkBGF;
+    private Camera tg;
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetActiveARCamera()
+    {
+        vumarkBGF.SetActive(true);
+        mainCamera.enabled = false;
+        cameraAR.SetActive(true);
+    }
+
+    public void CloseARCamera()
+    {
+        mainCamera.enabled = true;
+        vumarkBGF.SetActive(false);
+        cameraAR.SetActive(false);
+    }
+}
