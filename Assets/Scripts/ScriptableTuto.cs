47a9315cbe3c93d084f6030dc11aa855d8abe486
MadFox <otaku.suprem@gmail.com>
2018-10-24 15:26:29 +0200

    scene + element tuto

---
+++ Assets/Scripts/ScriptableTuto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Scriptable_Tuto", menuName = "Scriptable/Tuto", order = 1)]
+
+public class ScriptableTuto : ScriptableObject {
+
+    public Sprite[] imageBoard;
+    public string[] tutoTextBoard;
+
+}
--- Assets/Scripts/Tuto_Manager.cs
+++ Assets/Scripts/Tuto_Manager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Tuto_Manager : MonoBehaviour
 {
+
+    public ScriptableTuto currentScriptableTuto;
+
+
+
+
+
+
+
+
+
+
 //    [Header("Tuto")]
 
 //    public Transform tutoPannel;
@@ -45,97 +57,99 @@ public class Tuto_Manager : MonoBehaviour
 //    }
 //}
 
-	public Transform tutoB;
-    public Transform tutoS;
-    public Transform tutoI;
-    public Transform tutoG;
-    public GameObject arrowRight;
-    public GameObject arrowLeft;
-    public GameObject activeArrows;
+	//public Transform tutoB;
+ //   public Transform tutoS;
+ //   public Transform tutoI;
+ //   public Transform tutoG;
+ //   public GameObject arrowRight;
+ //   public GameObject arrowLeft;
+ //   public GameObject activeArrows;
 
-    private Transform currentTuto = null;
-    private int index = 0;
+ //   private Transform currentTuto = null;
+ //   private int index = 0;
 
-    private List<Transform> children = new List<Transform>();
+ //   private List<Transform> children = new List<Transform>();
 
-    public void ShowElement(GameObject elementToActive)
-    {
-        elementToActive.SetActive(true);
-    }
+ //   public void ShowElement(GameObject elementToActive)
+ //   {
+ //       elementToActive.SetActive(true);
+ //   }
 
-    public void UnShowElement(GameObject elementToDesactive)
-    {
-        elementToDesactive.SetActive(false);
-    }
+ //   public void UnShowElement(GameObject elementToDesactive)
+ //   {
+ //       elementToDesactive.SetActive(false);
+ //   }
 
-    public void ShowTuto(Transform tutoToShow)
-    {
-        if (currentTuto != null)
-        {
-            currentTuto.gameObject.SetActive(false);
-            children[index].gameObject.SetActive(false);
-            index = 0;
-            children[index].gameObject.SetActive(true);
-            arrowLeft.SetActive(false);
-            arrowRight.SetActive(true);
-        }
+ //   public void ShowTuto(Transform tutoToShow)
+ //   {
+ //       if (currentTuto != null)
+ //       {
+ //           currentTuto.gameObject.SetActive(false);
+ //           children[index].gameObject.SetActive(false);
+ //           index = 0;
+ //           children[index].gameObject.SetActive(true);
+ //           arrowLeft.SetActive(false);
+ //           arrowRight.SetActive(true);
+ //       }
 
-        index = 0;
-        children.Clear();
-        currentTuto = tutoToShow;
-        tutoToShow.gameObject.SetActive(true);
-        activeArrows.gameObject.SetActive(true);
+ //       index = 0;
+ //       children.Clear();
+ //       currentTuto = tutoToShow;
+ //       tutoToShow.gameObject.SetActive(true);
+ //       activeArrows.gameObject.SetActive(true);
 
-        for (int i = 0; i < currentTuto.childCount; i++)
-        {
-            children.Add(currentTuto.transform.GetChild(i));
-        }
-        Debug.Log("children" + children.Count);
-        Debug.Log(index);
-    }
+ //       for (int i = 0; i < currentTuto.childCount; i++)
+ //       {
+ //           children.Add(currentTuto.transform.GetChild(i));
+ //       }
+ //       Debug.Log("children" + children.Count);
+ //       Debug.Log(index);
+ //   }
 
-    public void UnShowTuto()
-    {
-        currentTuto.gameObject.SetActive(false);
-        activeArrows.gameObject.SetActive(false);
-        currentTuto = null;
-        index = 0;
-    }
+ //   public void UnShowTuto()
+ //   {
+ //       currentTuto.gameObject.SetActive(false);
+ //       activeArrows.gameObject.SetActive(false);
+ //       currentTuto = null;
+ //       index = 0;
+ //   }
 
-    public void NextTuto()
-    {
-        if (index < children.Count - 1)
-        {
-            currentTuto.GetChild(index).gameObject.SetActive(false);
-            index++;
-            currentTuto.GetChild(index).gameObject.SetActive(true);
-            arrowLeft.SetActive(true);
-            Debug.Log(index);
-        }
+ //   public void NextTuto()
+ //   {
+ //       if (index < children.Count - 1)
+ //       {
+ //           currentTuto.GetChild(index).gameObject.SetActive(false);
+ //           index++;
+ //           currentTuto.GetChild(index).gameObject.SetActive(true);
+ //           arrowLeft.SetActive(true);
+ //           Debug.Log(index);
+ //       }
 
-        if (index == children.Count - 1)
-        {
-            arrowRight.SetActive(false);
-        }
-    }
+ //       if (index == children.Count - 1)
+ //       {
+ //           arrowRight.SetActive(false);
+ //       }
+ //   }
 
-    public void PreviousTuto()
-    {
+ //   public void PreviousTuto()
+ //   {
 
-        if (index > 0)
-        {
-            currentTuto.GetChild(index).gameObject.SetActive(false);
+ //       if (index > 0)
+ //       {
+ //           currentTuto.GetChild(index).gameObject.SetActive(false);
 
-            index--;
-            currentTuto.GetChild(index).gameObject.SetActive(true);
-            arrowRight.SetActive(true);
-        }
+ //           index--;
+ //           currentTuto.GetChild(index).gameObject.SetActive(true);
+ //           arrowRight.SetActive(true);
+ //       }
 
-        if (index == 0)
-        {
-            arrowLeft.SetActive(false);
-        }
+ //       if (index == 0)
+ //       {
+ //           arrowLeft.SetActive(false);
+ //       }
 
-    }
+ //   }
+
+
 }
 
