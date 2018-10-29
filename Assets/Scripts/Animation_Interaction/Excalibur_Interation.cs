dd37741a095a66603bd3a4c12e01799b0ac923b2
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-10-29 09:35:27 +0100

    anim excalibur + interaction + scene arthur

---
+++ Assets/Scripts/Animation_Interaction/Excalibur_Interation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Excalibur_Interation : MonoBehaviour {
+
+    public Animator animExcalibur;
+    public int toDestroy; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        animExcalibur = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    //public void OnCollisoonEnter(Collision other)
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        toDestroy--;
+    //        Debug.Log(toDestroy);
+    //        if (toDestroy == 0)
+    //        {
+    //            animExcalibur.SetTrigger("isTaped");
+    //        }
+    //    }
+    //}
+
+    private void OnMouseDown()
+    {
+        
+        toDestroy--;
+        Debug.Log(toDestroy);
+
+        if(toDestroy == 0)
+        {
+            animExcalibur.SetBool("IsTaped", true);
+            Debug.Log("merde");
+        }
+    }
+}
