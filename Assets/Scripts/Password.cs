91cb68258cc58e707dbb5c7e5c52a8142732ee5d
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-11-15 16:25:40 +0100

    password

---
+++ Assets/Scripts/Password.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Password : MonoBehaviour {
+
+    public string passwordToPass = "zeldagoty";
+
+    public InputField passwordToEnter;
+    public Text maxInputString;
+
+    private string[] maskArray = new string[18];
+    private int maxIndex;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void PasswordToCheck()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            maxIndex--;
+            maxInputString.text = maskArray[maxIndex];
+        }
+        else
+        {
+            maxIndex++;
+            maxInputString.text = maskArray[maxIndex];
+        }
+        //if(passwordToPass == "zeldagoty")
+        //{
+        //    Debug.Log("BRAVO LE VEAU");
+        //}
+    }
+}
