9fdd35d08d14319005b8b76eaf11ad078fc404bf
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-11-26 14:47:14 +0100

    scriptable story

---
+++ Assets/Scripts/Scriptables/Scriptable_Story.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "StoryManager_Category", menuName = "Scriptable/Story Manager", order = 7)]
+
+public class Scriptable_Story : ScriptableObject {
+
+    public List<Image> scriptableStoryList;
+}
