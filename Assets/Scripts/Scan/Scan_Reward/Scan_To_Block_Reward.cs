c2a992811017007fc306531c235ec001c391427c
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-11-27 11:36:07 +0100

    tuto gallery + sauvegarde + interface tuto + button precedent

---
+++ Assets/Scripts/Scan/Scan_Reward/Scan_To_Block_Reward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan_To_Block_Reward : MonoBehaviour {
+
+    public string textToShow;
+    public List<bool> rewardAlreadyDone;
+
+	//void Start ()
+ //   {
+ //       Interface_Manager.Instance.RewardToBlock();
+	//}
+
+ //   public void RewardToBlock(int )
+ //   {
+ //       if(rewardAlreadyDone[] == true)
+ //       {
+
+ //       }
+ //       else if (rewardAlreadyDone[] == false)
+ //       {
+ //           rewardAlreadyDone[] = true;
+ //       }
+ //   }
+}
