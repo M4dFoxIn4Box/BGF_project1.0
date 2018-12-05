6a5f31a2c5d12f03edcd924d9bb5bf680489d837
MSI\Lucas <lucasblon@orange.fr>
2018-12-05 15:14:05 +0100

    inte mario

---
+++ Assets/Scripts/Animation_Interaction/Mario.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject fakeARObject;
+    public GameObject champignon;
+
+    // Use this for initialization
+    void Start () {
+        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        anim.SetBool("AnimPlay", true);
+    }
+
+    void ActiveChampi()
+    {
+        champignon.SetActive(true);
+    }
+}
