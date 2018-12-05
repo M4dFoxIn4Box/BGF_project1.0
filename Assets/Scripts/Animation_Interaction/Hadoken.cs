06b646aea70fbd65599f7c0959b22a2c5278357c
MSI\Lucas <lucasblon@orange.fr>
2018-12-05 10:35:15 +0100

    Hadoken

---
+++ Assets/Scripts/Animation_Interaction/Hadoken.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hadoken : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject fakeARObject;
+
+    // Use this for initialization
+    void Start () {
+
+        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
+        anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        anim.SetBool("Hadoken", true);
+        Debug.Log("chage state0");
+    }
+}
