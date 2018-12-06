6adf64ce9894099760a0965bbfd7a906c94d68fd
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-12-06 15:27:52 +0100

    audio inté

---
+++ Assets/Scripts/Audio/Audio_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour {
+
+    public AudioSource sfxManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SoundsToPlay (AudioClip currentSFX)
+    {
+        sfxManager.PlayOneShot(currentSFX);
+    }
+}
