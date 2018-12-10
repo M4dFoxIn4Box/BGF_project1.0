9cc636e013247ad55bbf9de7fa95548d8487c89e
MSI\Lucas <lucasblon@orange.fr>
2018-12-10 09:24:10 +0100

    pokeball

---
+++ Assets/Scripts/Animation_Interaction/PokemonPokeball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonPokeball : MonoBehaviour {
+
+    //public Animator pokemonAnim;
+    public Animator cameraAnim;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void CameraChangeView()
+    {
+        cameraAnim.SetBool("ChangeCamera", true);
+    }
+}
