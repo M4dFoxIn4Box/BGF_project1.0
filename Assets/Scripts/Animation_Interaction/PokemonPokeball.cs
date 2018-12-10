7d27ec0b2b50ff5b7f8148b298928c61adfe8c00
MSI\Lucas <lucasblon@orange.fr>
2018-12-10 15:44:45 +0100

    pokeball animation

--- Assets/Scripts/Animation_Interaction/PokemonPokeball.cs
+++ Assets/Scripts/Animation_Interaction/PokemonPokeball.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PokemonPokeball : MonoBehaviour {
 
-    //public Animator pokemonAnim;
+    public Animator pokemonAnim;
     public Animator cameraAnim;
+    public GameObject fakeARObject;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,8 +21,18 @@ public class PokemonPokeball : MonoBehaviour {
 	}
 
 
+    void OnMouseDown ()
+    {
+        pokemonAnim.SetBool("Lunchpokeball", true);
+    }
+
     void CameraChangeView()
     {
         cameraAnim.SetBool("ChangeCamera", true);
+    }
+
+    void Reverse()
+    {
+        cameraAnim.SetBool("ReverseBool", true);
     }
 }
