32251058ec998945df8a33b9ee430009475f1a5b
MSI\Lucas <lucasblon@orange.fr>
2018-12-11 14:39:25 +0100

    anim vfx

---
+++ Assets/Scripts/Animation_Interaction/Ken_Ryu/Ryu_Render_Texture_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Ryu_Render_Texture_Controller : MonoBehaviour {
+
+    public VideoPlayer tex;
+    public 
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Reset()
+    {
+        tex.frame = 0;
+        tex.Play();
+        Debug.Log(tex.frame);
+    }
+
+    IEnumerator resetVideo()
+    {
+        yield return new WaitForSeconds(0.01f);
+    }
+}
