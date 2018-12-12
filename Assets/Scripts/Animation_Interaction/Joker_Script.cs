471c7ccf7e2dc200ac35f52082727057d26f4387
MSI\Lucas <lucasblon@orange.fr>
2018-12-12 15:22:34 +0100

    inte build 01

---
+++ Assets/Scripts/Animation_Interaction/Joker_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joker_Script : MonoBehaviour {
+
+    public Animator jokerAnimator;
+    public GameObject fakeARObject;
+
+    // Use this for initialization
+    void Start () {
+        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        jokerAnimator.SetBool("Batarang", true);
+    }
+}
