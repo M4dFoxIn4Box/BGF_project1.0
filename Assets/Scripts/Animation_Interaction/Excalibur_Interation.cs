b2a552bbe9ca2587e51ac237dd3f67910b417e7c
Tentapoulpe <arthur.manoux@wanadoo.fr>
2018-12-13 17:11:37 +0100

    audio mixer Zelda

--- Assets/Scripts/Animation_Interaction/Excalibur_Interation.cs
+++ Assets/Scripts/Animation_Interaction/Excalibur_Interation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEngine.Audio;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +8,17 @@ public class Excalibur_Interation : MonoBehaviour {
     public Animator currentAnimation;
     private bool isOKTap = true;
     public GameObject fakeARObject;
+    public AudioClip[] audioZelda;
+    public AudioMixerGroup[] mixerZelda;
+    private int excaliburCount = 3;
     
 	// Use this for initialization
 	void Start ()
     {
-        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
+        //ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
         currentAnimation = GetComponent<Animator>();
-	}
+        excaliburCount = 3;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,14 +34,36 @@ public class Excalibur_Interation : MonoBehaviour {
     {
         if(isOKTap)
         {
-            isOKTap = false;
-            currentAnimation.SetTrigger("Excalibur_Make_Step");
+            Debug.Log(excaliburCount);
+            if (excaliburCount == 1)
+            {
+                currentAnimation.SetTrigger("Excalibur_Make_Step");
+                excaliburCount--;
+                Audio_Manager.audio.SoundsToPlay(audioZelda[1]);
+                Audio_Manager.audio.GetComponent<AudioSource>().outputAudioMixerGroup = mixerZelda[1];
+                StartCoroutine(GetExcalibur());
+            }
+            else if(excaliburCount >= 1)
+            {   excaliburCount--;
+                Audio_Manager.audio.SoundsToPlay(audioZelda[0]);
+                Audio_Manager.audio.GetComponent<AudioSource>().outputAudioMixerGroup = mixerZelda[0];
+                isOKTap = false;
+                currentAnimation.SetTrigger("Excalibur_Make_Step");
+            }
         }
     }
 
     public void PlayerCanTap()
     {
         isOKTap = true;
+    }
+
+    public IEnumerator GetExcalibur()
+    {
+        Debug.Log("YOOOOOOOOOOOOOOOOOOO");
+        yield return new WaitForSeconds(1.0f);
+        Audio_Manager.audio.SoundsToPlay(audioZelda[2]);
+        Audio_Manager.audio.GetComponent<AudioSource>().outputAudioMixerGroup = mixerZelda[2];
     }
 
     //public void ExcaliburIsFinished()
