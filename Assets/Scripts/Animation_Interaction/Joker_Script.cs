f9298bf5af97fbee23db936b57fb14471b225d18
MadFox <otaku.suprem@gmail.com>
2018-12-14 11:38:39 +0100

    script modif about fake ar to destroy

--- Assets/Scripts/Animation_Interaction/Joker_Script.cs
+++ Assets/Scripts/Animation_Interaction/Joker_Script.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class Joker_Script : MonoBehaviour {
 
     public Animator jokerAnimator;
-    public GameObject fakeARObject;
-
-    // Use this for initialization
-    void Start () {
-        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
-    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/Animation_Interaction/PokemonPokeball.cs
+++ Assets/Scripts/Animation_Interaction/PokemonPokeball.cs
@@ -7,16 +7,9 @@ public class PokemonPokeball : MonoBehaviour {
 
     public Animator pokemonAnim;
     public Animator cameraAnim;
-    public GameObject fakeARObject;
+
     public AudioClip[] audioPokemon;
     public AudioMixerGroup[] mixerPokemon;
-
-
-    // Use this for initialization
-    void Start () {
-
-        ScriptTracker.Instance.FakeARToDeactivate(fakeARObject);
-    }
 	
 	// Update is called once per frame
 	void Update () {
